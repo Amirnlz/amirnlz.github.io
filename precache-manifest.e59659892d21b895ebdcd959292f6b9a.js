a1ad649f2c863714f0f366f5dfed3222bbcf9482
Amir <amiirnlz79@gmail.com>
2024-10-27 00:25:32 +0330

    Updates

---
+++ precache-manifest.e59659892d21b895ebdcd959292f6b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112b099e40c2ef77dce9029b6bdc4aaf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "57f3bd73ada5b1547093",
+    "url": "/portfolio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "3d43db316123cdc30e24",
+    "url": "/portfolio/static/js/2.733b44c7.chunk.js"
+  },
+  {
+    "revision": "70c068b2fb9d959ad245",
+    "url": "/portfolio/static/js/3.730e5367.chunk.js"
+  },
+  {
+    "revision": "57f3bd73ada5b1547093",
+    "url": "/portfolio/static/js/main.789a9dca.chunk.js"
+  },
+  {
+    "revision": "942b3624d771dd960b08",
+    "url": "/portfolio/static/js/runtime~main.400d7f1e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  }
+]);
